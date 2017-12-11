d4cdc55fb1258d89d1ac8372dfc2e79f680b347b
Kuba <jakub.wajs10@gmail.com>
2017-12-11 23:28:18 +0100

    With update after 300 seconds

--- weather.sh
+++ weather.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
+#config
 base_request="http://api.apixu.com/v1/current.json"
 key="APIXU_KEY"
-location="Paris"
+tmp_data="./tmp/tmp.json"
+def_location="Poznan"
+update_time=300
 
-ready_request=$base_request"?key="$key"&q="$location
+#add location from parameter
+location=$def_location
 
-curl --create-dirs -o ./tmp/tmp.json $ready_request
+#check if ./tmp/tmp.json exists or when was last modification
+#and if location is the same!!!
+#if dynamic update with while and sleep
+
+if [ -f $tmp_data ]
+then
+    last_modif_time=$(($(date +%s) - $(date +%s -r $tmp_data)))
+else
+    last_modif_time=$(($update_time + 1))
+fi
+
+if [ $last_modif_time -gt $update_time ]
+then
+    ready_request=$base_request"?key="$key"&q="$location
+    curl --create-dirs -o $tmp_data $ready_request
+fi
+
+#show data
+cat $tmp_data | jq -r '.[] | "\(.location)\t\(.location)"
