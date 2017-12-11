6f54304afe17ec35586465e292948d039ba438e4
Kuba <jakub.wajs10@gmail.com>
2017-12-12 00:18:57 +0100

    With -l for location

--- weather.sh
+++ weather.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 #config
+APIXUKEY="APIXU_KEY"
 base_request="http://api.apixu.com/v1/current.json"
-key="APIXU_KEY"
 tmp_data="./tmp/tmp.json"
-def_location="Poznan"
+location="Poznan"
 update_time=300
+dynamic_update=0
 
-#add location from parameter
-location=$def_location
+#get parameters
+while getopts ":l:d" o; do
+    case "${o}" in
+        l)
+            location=${OPTARG}
+            ;;
+        d)
+            dynamic_update=1
+            ;;
+    esac
+done
 
 #check if ./tmp/tmp.json exists or when was last modification
 #and if location is the same!!!
@@ -23,9 +33,11 @@ fi
 
 if [ $last_modif_time -gt $update_time ]
 then
-    ready_request=$base_request"?key="$key"&q="$location
+    ready_request=$base_request"?key="$APIXUKEY"&q="$location
     curl --create-dirs -o $tmp_data $ready_request
 fi
 
 #show data
-cat $tmp_data | jq -r '.[] | "\(.location)\t\(.location)"
+file_data=$(jq . $tmp_data)
+
+echo $($file_data | jq .location)
